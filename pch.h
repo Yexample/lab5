53bc678578d9f3055e0afbb22c24f8f7880f515d
Yexample <65499484+Yexample@users.noreply.github.com>
2020-05-18 00:17:20 +0500

    Create pch.h

---
+++ pch.h
@@ -0,0 +1,18 @@
+#ifndef PCH_H
+#define PCH_H
+
+// TODO: add headers that you want to pre-compile here
+
+#endif //PCH_H
+#ifndef fu
+#define fu
+void fug();
+#endif 
+#ifndef matrix
+#define matrix
+void pmx();
+#endif 
+#ifndef vec
+#define vec
+void vect();
+#endif 
