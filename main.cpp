96b1a6bf0b4e79ba14298c2222e117407d405648
Yexample <65499484+Yexample@users.noreply.github.com>
2020-05-18 00:10:51 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include <iostream>
+
+int n;
+FILE *f1, *f2, *f3, *f4;
+char name1[90] = "\0", name2[90] = "\0", name3[90] = "\0", name4[90] = "\0";
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(CP_UTF8);
+	pmx();
+	vect();
+	fug();
+	return 0;
+}
